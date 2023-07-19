22894de95c93cf5c1f01facaf5c4bd6f61d14b0c
PhindileT <139931911+PhindileT@users.noreply.github.com>
2023-07-19 02:46:00 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,18 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+
+public class BasicFrameApp {
+
+   
+    public static void main(String[] args) {
+        //Declare variable
+        MyFirstFrame myFrame;
+        
+        //instantiate the frame
+        myFrame = new MyFirstFrame();
+    }
+    
+}
